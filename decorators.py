ce746911dd1560c1b3430de1fd99ec5d6db935af
Eric Bower <neurosnap@gmail.com>
2014-08-26 22:09:15 -0400

    updated logic

--- decorators.py
+++ decorators.py
@@ -5,6 +5,15 @@ from functools import wraps
 from flask import request, render_template, jsonify
 from ua_parser import user_agent_parser
 
+def not_browser(user_agent, format_as):
+    ua = user_agent_parser.Parse(user_agent)
+    if (ua['os']['family'] == "Other"
+        or ua['user_agent']['family'] == "Other"
+        or format_as == "json"):
+            return True
+
+    return False
+
 def flask_api(template="api.html", title="Some API Title", **api_kwargs):
     """ Primary abstraction to simplify the web browsable API
     It figures out where the request is coming from -- e.g. browser, curl -- and
@@ -15,21 +24,17 @@ def flask_api(template="api.html", title="Some API Title", **api_kwargs):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-
-            format_as = request.args.get("format")
-
             func_val = func(*args, **kwargs)
             response = json.dumps(func_val, sort_keys=True, indent=2,
                                     separators=(',', ': '))
             req = "%s %s" % (request.method,
                             request.path.replace(" ", "%20"))
-            user_agent = user_agent_parser.Parse(
-                            request.headers.get("User-Agent"))
 
-            if (format_as == "json"
-                or user_agent['os']['family'] == "Other"
-                or user_agent['user_agent']['family'] == "Other"):
-                    return jsonify(**func_val)
+            user_agent = request.headers.get("User-Agent")
+            format_as = request.args.get("format")
+
+            if not_browser(user_agent, format_as):
+                return jsonify(**func_val)
             else:
                 return render_template(template, request=req,
                                         response=response, title=title,
